8b2236ff3789f4f956fb5b5ffd9b7cc205489c52
sulenq <whyimustwriteit@gmail.com>
2024-08-19 16:12:16 +0700

    api cuti

--- src/components/independent/ListCuti.tsx
+++ src/components/independent/ListCuti.tsx
@@ -1,89 +1,113 @@
-import { Button, HStack, SimpleGrid, StackProps, Text } from "@chakra-ui/react";
+import { Button, Center, HStack, SimpleGrid, Text } from "@chakra-ui/react";
 import { useLightDarkColor } from "../../constant/colors";
-import { Interface__Cuti } from "../../constant/interfaces";
+import useDataState from "../../hooks/useDataState";
 import formatDate from "../../lib/formatDate";
 import BooleanBadge from "../dependent/BooleanBadge";
 import Skeleton from "./Skeleton";
 import CContainer from "./wrapper/CContainer";
+import useFilterCuti from "../../global/useFilterCuti";
+import NoData from "./NoData";
+import Retry from "../dependent/Retry";
 
-interface Props extends StackProps {
-  loading: boolean;
-  data: Interface__Cuti[];
-}
-
-export default function ListCuti({ loading, data, ...props }: Props) {
+export default function ListCuti() {
   // SX
   const lightDarkColor = useLightDarkColor();
 
+  const { filterCuti } = useFilterCuti();
+
+  const { error, notFound, loading, data, retry } = useDataState<any>({
+    initialData: undefined,
+    url: `/api/get-riwayat-cuti`,
+    payload: { ...filterCuti },
+    dependencies: [filterCuti],
+  });
+
   return (
-    <CContainer gap={3} {...props}>
+    <CContainer gap={3} flex={1}>
       {loading &&
         Array.from({ length: 10 }).map((cuti, i) => (
           <Skeleton key={i} h={"185px"} />
         ))}
 
-      {!loading && data && (
+      {!loading && (
         <>
-          {data.map((cuti, i) => (
-            <CContainer
-              key={i}
-              p={4}
-              borderRadius={12}
-              bg={lightDarkColor}
-              gap={3}
-            >
-              <HStack justify={"space-between"} align={"start"}>
-                <CContainer gap={1}>
-                  <Text opacity={0.4} fontSize={12}>
-                    Kategori Cuti
-                  </Text>
-                  <Text fontWeight={500}>{cuti.kategori.label}</Text>
-                </CContainer>
+          {error && (
+            <>
+              {notFound && <NoData minH={"132px"} label="Tidak ada jadwal" />}
 
-                <BooleanBadge
-                  data={cuti.status_cuti}
-                  trueValue="Disetujui"
-                  falseValue="Tidak DIsetujui"
-                  borderRadius={"full"}
-                />
-              </HStack>
+              {!notFound && (
+                <Center my={"auto"} minH={"300px"}>
+                  <Retry loading={loading} retry={retry} />
+                </Center>
+              )}
+            </>
+          )}
 
-              <CContainer gap={1}>
-                <Text opacity={0.4} fontSize={12}>
-                  Tanggal Cuti
-                </Text>
-                <Text fontWeight={500}>{`${formatDate(
-                  cuti.tanggal_from,
-                  "basicShort"
-                )} - ${formatDate(cuti.tanggal_to, "basicShort")}`}</Text>
-              </CContainer>
+          {!error && (
+            <>
+              {data &&
+                data?.data?.map((cuti: any, i: number) => (
+                  <CContainer
+                    key={i}
+                    p={4}
+                    borderRadius={12}
+                    bg={lightDarkColor}
+                    gap={3}
+                  >
+                    <HStack justify={"space-between"} align={"start"}>
+                      <CContainer gap={1}>
+                        <Text opacity={0.4} fontSize={12}>
+                          Kategori Cuti
+                        </Text>
+                        <Text fontWeight={500}>{cuti.kategori.label}</Text>
+                      </CContainer>
 
-              <SimpleGrid columns={2} gap={6}>
-                <CContainer gap={1}>
-                  <Text opacity={0.4} fontSize={12}>
-                    Total Cuti
-                  </Text>
-                  <Text fontWeight={500}>{cuti.durasi} Hari</Text>
-                </CContainer>
+                      <BooleanBadge
+                        data={cuti.status_cuti}
+                        trueValue="Disetujui"
+                        falseValue="Tidak DIsetujui"
+                        borderRadius={"full"}
+                      />
+                    </HStack>
 
-                <CContainer gap={1}>
-                  <Text opacity={0.4} fontSize={12}>
-                    Disetujui oleh
-                  </Text>
-                  <Text fontWeight={500}>{"-"}</Text>
-                </CContainer>
-              </SimpleGrid>
-            </CContainer>
-          ))}
+                    <CContainer gap={1}>
+                      <Text opacity={0.4} fontSize={12}>
+                        Tanggal Cuti
+                      </Text>
+                      <Text fontWeight={500}>{`${formatDate(
+                        cuti.tanggal_from,
+                        "basicShort"
+                      )} - ${formatDate(cuti.tanggal_to, "basicShort")}`}</Text>
+                    </CContainer>
 
-          <Button
-            flexShrink={0}
-            colorScheme="ap"
-            variant={"ghost"}
-            className="clicky"
-          >
-            Tampilkan Lebih Banyak
-          </Button>
+                    <SimpleGrid columns={2} gap={6}>
+                      <CContainer gap={1}>
+                        <Text opacity={0.4} fontSize={12}>
+                          Total Cuti
+                        </Text>
+                        <Text fontWeight={500}>{cuti.durasi} Hari</Text>
+                      </CContainer>
+
+                      <CContainer gap={1}>
+                        <Text opacity={0.4} fontSize={12}>
+                          Disetujui oleh
+                        </Text>
+                        <Text fontWeight={500}>{"-"}</Text>
+                      </CContainer>
+                    </SimpleGrid>
+                  </CContainer>
+                ))}
+
+              <Button
+                flexShrink={0}
+                colorScheme="ap"
+                variant={"ghost"}
+                className="clicky"
+              >
+                Tampilkan Lebih Banyak
+              </Button>
+            </>
+          )}
         </>
       )}
     </CContainer>
--- src/pages/main/Beranda/Cuti.tsx
+++ src/pages/main/Beranda/Cuti.tsx
@@ -1,92 +1,22 @@
 import { Box, Text } from "@chakra-ui/react";
 import Header from "../../../components/dependent/Header";
-import Retry from "../../../components/dependent/Retry";
 import StatistikCuti from "../../../components/dependent/StatistikCuti";
 import AjukanCuti from "../../../components/independent/AjukanCuti";
 import FilterCuti from "../../../components/independent/FilterCuti";
 import ListCuti from "../../../components/independent/ListCuti";
 import CContainer from "../../../components/independent/wrapper/CContainer";
 import { useContentBgColor, useLightDarkColor } from "../../../constant/colors";
-import { dummyCutis } from "../../../constant/dummy";
-import useDataState from "../../../hooks/useDataState";
 import useScrollToTop from "../../../hooks/useScrollToTop";
-import NoData from "../../../components/independent/NoData";
 
 export default function Cuti() {
   useScrollToTop();
 
-  const dummy = {
-    statistik: [
-      {
-        nama: "Cuti Tahunan",
-        terpakai: 4,
-        kuota: 12, // kuota pertahun
-        is_need_requirement: false,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti di Luar Tanggungan",
-        terpakai: 5,
-        kuota: 30, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti Besar",
-        terpakai: 7,
-        kuota: 12, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti Kelahiran",
-        terpakai: 0,
-        kuota: 90, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti Sakit",
-        terpakai: 2,
-        kuota: null, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti Nikah",
-        terpakai: 0,
-        kuota: null, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-      {
-        nama: "Cuti Kematian",
-        terpakai: 1,
-        kuota: null, // kuota pertahun
-        is_need_requirement: true,
-        keterangan:
-          "Keterangan entitas cuti, berupa deskripsi cuti, diperuntukan untuk apa, dan dapat disertakan syarat - syarat cuti disini, dapat diedit ",
-      },
-    ],
-    riwayat: dummyCutis,
-  };
-
-  const { error, loading, data, retry } = useDataState<any>({
-    initialData: dummy,
-    url: "",
-  });
   // SX
   const lightDarkColor = useLightDarkColor();
   const contentBgColor = useContentBgColor();
 
   return (
-    <CContainer>
+    <CContainer flex={1}>
       <Box
         position={"sticky"}
         top={"0"}
@@ -99,34 +29,18 @@ export default function Cuti() {
         <FilterCuti />
       </Box>
 
-      <CContainer p={5} pb={8} bg={contentBgColor}>
-        {error && (
-          <Box my={"auto"}>
-            <Retry loading={loading} retry={retry} />
-          </Box>
-        )}
+      <CContainer flex={1} p={5} pb={8} bg={contentBgColor}>
+        <Text mx={1} fontWeight={600} mb={2}>
+          Statistik Cuti
+        </Text>
 
-        {!error && (
-          <>
-            {!data && <NoData />}
+        <StatistikCuti />
 
-            {data && (
-              <>
-                <Text mx={1} fontWeight={600} mb={2}>
-                  Statistik Cuti
-                </Text>
+        <Text mx={1} fontWeight={600} mt={5} mb={2}>
+          Riwayat
+        </Text>
 
-                <StatistikCuti  />
-
-                <Text mx={1} fontWeight={600} mt={5} mb={2}>
-                  Riwayat
-                </Text>
-
-                <ListCuti loading={loading} data={data.riwayat} />
-              </>
-            )}
-          </>
-        )}
+        <ListCuti />
       </CContainer>
 
       <AjukanCuti />
