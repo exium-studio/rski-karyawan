0bbdfd5fe11c55fbf5bca853a4e4f71941776a02
sulenq <whyimustwriteit@gmail.com>
2024-08-27 11:40:08 +0700

    styling

--- src/components/independent/MiniProfile.tsx
+++ src/components/independent/MiniProfile.tsx
@@ -6,12 +6,6 @@ import {
   HStack,
   Icon,
   IconButton,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   StackProps,
   Text,
   useDisclosure,
@@ -25,22 +19,21 @@ import {
 import { useErrorAlphaColor } from "../../constant/colors";
 import { Interface__User } from "../../constant/interfaces";
 import { responsiveSpacing } from "../../constant/sizes";
-import useBackOnClose from "../../hooks/useBackOnClose";
 import backOnClose from "../../lib/backOnClose";
-import DisclosureHeader from "../dependent/DisclosureHeader";
 import CContainer from "./wrapper/CContainer";
+import CustomDrawer from "./wrapper/CustomDrawer";
+import DisclosureHeader from "../dependent/DisclosureHeader";
 
 interface Props extends StackProps {
   data: Interface__User;
 }
 
-const InfoModal = () => {
+const InfoPresensi = () => {
   // SX
   // const errorColor = useErrorColor();
   const errorAlphaColor = useErrorAlphaColor();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  useBackOnClose(`info-modal`, isOpen, onOpen, onClose);
 
   return (
     <>
@@ -48,90 +41,106 @@ const InfoModal = () => {
         aria-label="info"
         icon={<Icon as={RiInformationLine} fontSize={24} />}
         borderRadius={"full"}
-        className="btn clicky"
+        className="clicky"
+        colorScheme="whiteAlpha"
+        variant={"ghost"}
         ml={"auto"}
+        color={"white"}
         onClick={onOpen}
       />
 
-      <Modal
+      <CustomDrawer
+        id="info-modal"
         isOpen={isOpen}
-        onClose={backOnClose}
-        isCentered
-        blockScrollOnMount={false}
+        onOpen={onOpen}
+        onClose={onClose}
+        header={<DisclosureHeader title="Info Aktivitas Presensi" />}
+        footer={
+          <Button w={"100%"} className="btn-solid clicky" onClick={backOnClose}>
+            Mengerti
+          </Button>
+        }
       >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>
-            <DisclosureHeader title={"Info"} />
-          </ModalHeader>
-          <ModalBody>
-            <CContainer gap={responsiveSpacing}>
-              <HStack gap={4}>
-                <Center
-                  w={"fit-content"}
-                  p={3}
-                  borderRadius={"full"}
-                  bg={"var(--p500a5)"}
-                >
-                  <Icon as={RiLoginBoxLine} fontSize={52} color={"p.500"} />
-                </Center>
-                <CContainer gap={2}>
-                  <Text fontWeight={500}>Presensi Masuk</Text>
-                  <Text opacity={0.4} fontSize={12}>
-                    Tombol ini digunakan untuk presensi masuk dan hanya muncul
-                    jika Anda belum melakukan presensi hari ini.
-                  </Text>
-                </CContainer>
-              </HStack>
-
-              <HStack gap={4}>
-                <Center
-                  w={"fit-content"}
-                  p={3}
-                  borderRadius={"full"}
-                  bg={errorAlphaColor}
-                >
-                  <Icon as={RiLogoutBoxLine} fontSize={52} color={"red.500"} />
-                </Center>
-                <CContainer gap={2}>
-                  <Text fontWeight={500}>Presensi Keluar</Text>
-                  <Text opacity={0.4} fontSize={12}>
-                    Tombol ini muncul untuk presensi keluar setelah Anda
-                    melakukan presensi masuk.
-                  </Text>
-                </CContainer>
-              </HStack>
-
-              <HStack gap={4}>
-                <Center
-                  w={"fit-content"}
-                  p={3}
-                  borderRadius={"full"}
-                  bg={"var(--divider2)"}
-                >
-                  <Icon as={RiCalendarCloseLine} fontSize={52} />
-                </Center>
-                <CContainer gap={2}>
-                  <Text fontWeight={500}>Tidak Ada Jadwal</Text>
-                  <Text opacity={0.4} fontSize={12}>
-                    Tombol ini muncul jika Anda tidak memiliki jadwal untuk hari
-                    ini.
-                  </Text>
-                </CContainer>
-              </HStack>
-            </CContainer>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              w={"100%"}
-              className="btn-solid clicky"
-              onClick={backOnClose}
+        <CContainer px={6} gap={responsiveSpacing}>
+          <HStack gap={4}>
+            <Center
+              bg={"white"}
+              borderRadius={16}
+              p={2}
+              border={"1px solid var(--divider3)"}
+              // boxShadow={"md"}
             >
-              Mengerti
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+              <Center
+                w={"fit-content"}
+                p={3}
+                borderRadius={"full"}
+                bg={"var(--p500a5)"}
+              >
+                <Icon as={RiLoginBoxLine} fontSize={52} color={"p.500"} />
+              </Center>
+            </Center>
+            <CContainer gap={2}>
+              <Text fontWeight={500}>Presensi Masuk</Text>
+              <Text opacity={0.4} fontSize={12}>
+                Tombol ini digunakan untuk presensi masuk dan hanya muncul jika
+                Anda belum melakukan presensi hari ini.
+              </Text>
+            </CContainer>
+          </HStack>
+
+          <HStack gap={4}>
+            <Center
+              bg={"white"}
+              borderRadius={16}
+              p={2}
+              border={"1px solid var(--divider3)"}
+              // boxShadow={"md"}
+            >
+              <Center
+                w={"fit-content"}
+                p={3}
+                borderRadius={"full"}
+                bg={errorAlphaColor}
+              >
+                <Icon as={RiLogoutBoxLine} fontSize={52} color={"red.500"} />
+              </Center>
+            </Center>
+            <CContainer gap={2}>
+              <Text fontWeight={500}>Presensi Keluar</Text>
+              <Text opacity={0.4} fontSize={12}>
+                Tombol ini muncul untuk presensi keluar setelah Anda melakukan
+                presensi masuk.
+              </Text>
+            </CContainer>
+          </HStack>
+
+          <HStack gap={4}>
+            <Center
+              bg={"white"}
+              borderRadius={16}
+              p={2}
+              border={"1px solid var(--divider3)"}
+              // boxShadow={"md"}
+            >
+              <Center
+                w={"fit-content"}
+                p={3}
+                borderRadius={"full"}
+                bg={"var(--divider)"}
+              >
+                <Icon as={RiCalendarCloseLine} fontSize={52} color={"dark"} />
+              </Center>
+            </Center>
+            <CContainer gap={2}>
+              <Text fontWeight={500}>Tidak Ada Jadwal</Text>
+              <Text opacity={0.4} fontSize={12}>
+                Tombol ini muncul jika Anda tidak memiliki jadwal untuk hari
+                ini.
+              </Text>
+            </CContainer>
+          </HStack>
+        </CContainer>
+      </CustomDrawer>
     </>
   );
 };
@@ -149,7 +158,7 @@ export default function MiniProfile({ data, ...props }: Props) {
         </Text>
       </Box>
 
-      <InfoModal />
+      <InfoPresensi />
     </HStack>
   );
 }
