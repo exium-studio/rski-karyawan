8997c99ddc33e3707199655051b027372c417551
sulenq <whyimustwriteit@gmail.com>
2024-08-29 21:07:24 +0700

    styling

--- src/components/independent/wrapper/CustomDrawer.tsx
+++ src/components/independent/wrapper/CustomDrawer.tsx
@@ -148,7 +148,6 @@ export default function CustomDrawer({
       initialFocusRef={initialRef}
       placement={placement}
       size={size}
-      blockScrollOnMount={false}
     >
       <DrawerOverlay />
       <DrawerContent
