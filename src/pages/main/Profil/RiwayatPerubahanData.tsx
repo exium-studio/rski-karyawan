3991fe14d23ef4e8898e59529c96e2a244086ae0
sulenq <whyimustwriteit@gmail.com>
2024-09-01 21:49:12 +0700

    styling

--- src/pages/main/Profil/RiwayatPerubahanData.tsx
+++ src/pages/main/Profil/RiwayatPerubahanData.tsx
@@ -9,62 +9,9 @@ import { useContentBgColor } from "../../../constant/colors";
 import useDataState from "../../../hooks/useDataState";
 
 export default function RiwayatPerubahanData() {
-  const dummy = [
-    {
-      id: 1,
-      kolom: "tgl_lahir",
-      original_data: "2001-11-01",
-      updated_data: "2001-11-05",
-      status_perubahan: null,
-      tgl_disetujui: "2024-07-11",
-      created_at: "2024-07-10",
-      updated_at: null,
-    },
-    {
-      id: 2,
-      kolom: "foto_profil",
-      original_data: "/images/reza.jpg",
-      updated_data: "/images/gear5.jpg",
-      status_perubahan: false,
-      tgl_disetujui: "2024-07-11",
-      created_at: "2024-07-10",
-      updated_at: null,
-    },
-    {
-      id: 3,
-      kolom: "tinggi_badan",
-      original_data: 170,
-      updated_data: 172,
-      status_perubahan: true,
-      tgl_disetujui: "2024-07-11",
-      created_at: "2024-07-10",
-      updated_at: null,
-    },
-    {
-      id: 5,
-      kolom: "agama",
-      original_data: { id: 1, label: "Islam" },
-      updated_data: { id: 2, label: "Kristen" },
-      status_perubahan: true,
-      tgl_disetujui: "2024-07-11",
-      created_at: "2024-07-10",
-      updated_at: null,
-    },
-    {
-      id: 4,
-      kolom: "ktp",
-      original_data: "/images/gear5.jpg",
-      updated_data: "/images/gear5.jpg",
-      status_perubahan: true,
-      tgl_disetujui: "2024-07-11",
-      created_at: "2024-07-10",
-      updated_at: null,
-    },
-  ];
-
   const { error, loading, data, retry } = useDataState<any>({
-    initialData: dummy,
-    url: "",
+    initialData: undefined,
+    url: `/api/get-riwayat-perubahan`,
     dependencies: [],
   });
 
@@ -72,7 +19,7 @@ export default function RiwayatPerubahanData() {
   const contentBgColor = useContentBgColor();
 
   return (
-    <CContainer>
+    <CContainer flex={1}>
       <Header
         left={"back"}
         title="Riwayat Perubahan Data"
@@ -80,7 +27,7 @@ export default function RiwayatPerubahanData() {
         borderBottom={"1px solid var(--divider2)"}
       />
 
-      <CContainer bg={contentBgColor} p={5} gap={3}>
+      <CContainer bg={contentBgColor} p={5} gap={3} flex={1}>
         {error && (
           <Box my={"auto"}>
             <Retry loading={loading} retry={retry} />
