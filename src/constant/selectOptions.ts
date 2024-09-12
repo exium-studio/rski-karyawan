cc03e03f69911dacf48f22777c1e70378087d058
sulenq <whyimustwriteit@gmail.com>
2024-09-12 09:18:21 +0700

    fix diksi khatolik

--- src/constant/selectOptions.ts
+++ src/constant/selectOptions.ts
@@ -1,7 +1,7 @@
 const optionsAgama = [
   { value: 1, label: "Islam" },
   { value: 2, label: "Kristen" },
-  { value: 3, label: "Katolik" },
+  { value: 3, label: "Khatolik" },
   { value: 4, label: "Budha" },
   { value: 5, label: "Hindu" },
   { value: 6, label: "Konghucu" },
