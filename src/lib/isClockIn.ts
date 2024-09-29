7ae5f2592638f0ae286146ee36804377d1a1c839
rezahawari <rezahawari19@gmail.com>
2024-09-30 06:16:23 +0700

    ketinggalan

--- src/lib/isClockIn.ts
+++ src/lib/isClockIn.ts
@@ -1,20 +1,16 @@
 export default function isClockIn(waktuMasuk: string): boolean {
-  if(waktuMasuk) {
-    const [jam, menit, detik] = waktuMasuk.split(":").map(Number);
-    const sekarang = new Date();
-    const waktuMasukObj = new Date(
-      sekarang.getFullYear(),
-      sekarang.getMonth(),
-      sekarang.getDate(),
-      jam,
-      menit,
-      detik
-    );
-  
-    return sekarang >= waktuMasukObj;
-  }else {
-    return false;
-  }
+  const [jam, menit, detik] = waktuMasuk.split(":").map(Number);
+  const sekarang = new Date();
+  const waktuMasukObj = new Date(
+    sekarang.getFullYear(),
+    sekarang.getMonth(),
+    sekarang.getDate(),
+    jam,
+    menit,
+    detik
+  );
+
+  return sekarang >= waktuMasukObj;
 
   // Buat objek Date dengan tanggal saat ini, tapi gunakan jam, menit, dan detik dari waktuMasuk
 }
