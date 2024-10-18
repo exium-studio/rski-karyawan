9a672c61cfb8e1bcebf4d945f9ff600fa46d8ffd
sulenq <whyimustwriteit@gmail.com>
2024-10-18 16:00:53 +0700

    fox logour

--- src/lib/logout.ts
+++ src/lib/logout.ts
@@ -1,6 +1,4 @@
-import { removeCookie } from "typescript-cookie";
-
 export default function logout() {
-  removeCookie("__auth_token");
+  localStorage.removeItem("__auth_token");
   localStorage.removeItem("__user_data");
 }
