add0825b3d72b8469b2cd4835d4d601ad7284bb6
sulenq <fatwalinovera@gmail.com>
2025-04-22 09:51:08 +0700

    refactor: add catch error get user data

--- src/lib/getUserData.ts
+++ src/lib/getUserData.ts
@@ -1,3 +1,18 @@
 export default function getUserData() {
-  return JSON.parse(localStorage.getItem("__user_data") as string);
+  const storedData = localStorage.getItem("__user_data") as string;
+  // console.log(storedData);
+
+  let data = null;
+  try {
+    if (storedData) {
+      data = JSON.parse(storedData);
+    }
+  } catch (error) {
+    localStorage.removeItem("__auth_token");
+    localStorage.removeItem("__user_data");
+  }
+
+  return data;
+
+  // return JSON.parse(storedData);
 }
